591937e37a8e1c72c732fa200070b7d361b123f1
Kent C. Dodds <kent.dodds@domo.com>
2013-09-26 21:27:28 -0600

    Got firebase authentication working. Moved a few things around. Adding angular-strap.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-angular.module('fl', []);
+angular.module('fl', ['firebase', '$strap.directives']);
 
 // Using old version of angular that doesn't have the ngKeyup directive built in.
 angular.module('fl').directive('ngKeyup', function($parse) {
@@ -12,7 +12,7 @@ angular.module('fl').directive('ngKeyup', function($parse) {
   };
 });
 
-angular.module('fl').controller('MainCtrl', function($scope, $filter) {
+angular.module('fl').controller('MainCtrl', function($scope, angularFireAuth, $filter) {
   var ref, dataWatcher, columnWatcher;
   
   $scope.data = getObjectInStorage('data') || {
@@ -22,32 +22,42 @@ angular.module('fl').controller('MainCtrl', function($scope, $filter) {
   };
   $scope.column = getObjectInStorage('column', true) || 0;
   $scope.dataToImport = getObjectInStorage('importData') || '';
-
+  
+  
+  ref = new Firebase('https://filtered-list.firebaseio.com/');
+  angularFireAuth.initialize(ref, {
+    scope: $scope, name: 'user',
+    callback: function(err, user) {
+      console.log('Authenticated!');
+      console.log(arguments);
+    }
+  });
+  
   function setObjectInStorage(itemName, object, noJson) {
     if (localStorage) {
       var valToSet = noJson ? object : JSON.stringify(object)
       localStorage.setItem(itemName, valToSet);
     }
   }
-
+  
   function getObjectInStorage(itemName, noJson) {
     if (localStorage) {
       return noJson ? localStorage.getItem(itemName) : JSON.parse(localStorage.getItem(itemName));
     }
   }
-
+  
   function removeObjectInStorage(itemName) {
     if (localStorage) {
       localStorage.removeItem(itemName);
     }
   }
-
+  
   $scope.importData = function() {
     $scope.data.toAdd = CSV.parse($scope.dataToImport, {headers: true});
     $scope.data.headers = $scope.data.toAdd.headers;
     setObjectInStorage('importData', $scope.dataToImport);
   }
-
+  
   $scope.moveItem = function(item, from, to) {
     if (!item) {
       return;
@@ -55,13 +65,13 @@ angular.module('fl').controller('MainCtrl', function($scope, $filter) {
     $scope.data[to].unshift(item);
     $scope.data[from].splice($scope.data[from].indexOf(item), 1);
   }
-
+  
   $scope.exportData = function() {
     $scope.dataToExport = CSV.stringify([$scope.data.headers].concat($scope.data.added), {
       forceQuotes: true
     });
   }
-
+  
   $scope.keyboardMoveItem = function(event, searchProp, from, to) {
     if (event.keyCode === 13) {
       var item = $filter('filter')($scope.data[from], $scope[searchProp])[0];
@@ -73,7 +83,7 @@ angular.module('fl').controller('MainCtrl', function($scope, $filter) {
       $scope[searchProp] = '';
     }
   }
-
+  
   $scope.clearMostData = function() {
     if (confirm('Have you exported the data?')) {
       removeObjectInStorage('data');
@@ -84,7 +94,7 @@ angular.module('fl').controller('MainCtrl', function($scope, $filter) {
       };
     }
   }
-
+  
   $scope.clearAllData = function() {
     if (confirm('Have you exported the data?')) {
       removeObjectInStorage('data');
@@ -99,12 +109,24 @@ angular.module('fl').controller('MainCtrl', function($scope, $filter) {
       $scope.dataToImport = '';
     }
   }
-
+  
   dataWatcher = $scope.$watch('data', function(newVal) {
     setObjectInStorage('data', newVal);
   }, true);
-
+  
   columnWatcher = $scope.$watch('column', function(newVal) {
     setObjectInStorage('column', newVal, true);
   });
+  
+  $scope.login = function(email, password) {
+    angularFireAuth.login('password', {
+      email: email,
+      password: password
+    });
+  }
+  
+  $scope.logout = function() {
+    angularFireAuth.logout();
+  }
+  
 });
